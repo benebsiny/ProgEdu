79ff18c56d1cbbbe602de15d86e23955f0ea3147
hoky <hoky841023@gmail.com>
2020-03-03 14:38:47 +0800

    save

--- server/src/test/java/fcu/selab/progedu/db/TestGroupCommitRecordDb.java
+++ server/src/test/java/fcu/selab/progedu/db/TestGroupCommitRecordDb.java
@@ -1,6 +1,10 @@
 package fcu.selab.progedu.db;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import fcu.selab.progedu.data.StudentCommitRecord;
 
 public class TestGroupCommitRecordDb {
   ProjectCommitRecordDbManager crdm = new ProjectCommitRecordDbManager();
@@ -8,7 +12,19 @@ public class TestGroupCommitRecordDb {
   @Test
   public void testCommitRecordDb() {
 //    crdm.getProjectCommitCount(1);
-    crdm.getCommitStatusPerStudent();
+    List<StudentCommitRecord> scrs = crdm.getCommitStatusPerStudent();
+//    crdm.getCommitStatusPerStudent();
+    crdm.getCommitFrequencyPerStudent(scrs);
 
+    System.out.println(scrs.size());
+    try {
+      for (StudentCommitRecord scr : scrs) {
+        System.out.println(scr.toString());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+//    System.out.print(50 % 3);
   }
 }
